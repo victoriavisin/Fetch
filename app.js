a7308ef314d63fe27df0e869841b061272575f1e
victoria.visin <vvisintin18@gmail.com>
2023-01-18 10:41:11 -0300

    otra prueba

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 let carrito = [];
 let total = 0;
-const contenedorProd = document.querySelector (".contenedor")
 const producto = document.querySelector (".card")
 
 
+const url = "./datos.json"
+
+fetch (url)
+.then (res=> res.json ())
+.then (data => mostrarProductos (data))
 
 
-fetch("./datos.json")
- .then((res) => res.json())
- .then((stockProductos) => {
-     // AGREGO MIS PRODUCTOS AL HTML
-    stockProductos.forEach ((prod) => {
-      contenedor.innerHTML += `
+const contenedorProd = document.querySelector (".contenedor")
+
+function mostrarProductos (productos) {
+  productos.forEach (prod => {
+    let card = document.createElement ('div')
+    card.innerHTML = `
     <div class="card mt-3" style="width: 18rem;">
     <img class="card-img-top mt-3" src="${prod.img}" alt="Card image cap">
     <div class="card-body text-center">
@@ -22,36 +26,59 @@ fetch("./datos.json")
     </div>
   </div>
   `;
+  contenedorProd.appendChild (card)
+  })
+}
+
+
+
+
+// fetch("./datos.json")
+//  .then((res) => res.json())
+//  .then((stockProductos) => {
+//      // AGREGO MIS PRODUCTOS AL HTML
+//     stockProductos.forEach ((prod) => {
+//       contenedor.innerHTML += `
+//     <div class="card mt-3" style="width: 18rem;">
+//     <img class="card-img-top mt-3" src="${prod.img}" alt="Card image cap">
+//     <div class="card-body text-center">
+//       <h5 class="card-title">${prod.nombre}</h5>
+//       <p class="card-text">Precio: $ ${prod.precio}</p>
+//       <p class="card-text-desc">Descripcion: ${prod.desc}</p>
+//       <button class="btn-comprar btn-warning" id= "(${prod.id})" type= "button">Comprar Producto</button>
+//     </div>
+//   </div>
+//   `;
   
  
-})
+// })
 
-//console.log (stockProductos)
+// //console.log (stockProductos)
  
 
-let agregarBtns = document.querySelectorAll (".btn-comprar")
-agregarBtns =  [...agregarBtns]
+// let agregarBtns = document.querySelectorAll (".btn-comprar")
+// agregarBtns =  [...agregarBtns]
 
-const modalBody = document.querySelector (".modal-body")
-
-
-
-agregarBtns.forEach (btn =>  {
-    btn.addEventListener ('click',(e)=> {
-        //console.log ('click')
+// const modalBody = document.querySelector (".modal-body")
 
 
-        //BUSCO ID DEL PRODUCTO
 
-        let actualId = (e.target.id)
-        console.log (actualId)
+// agregarBtns.forEach (btn =>  {
+//     btn.addEventListener ('click',(e)=> {
+//         //console.log ('click')
 
 
-        //ENCONTRAR OBJETO ACTUAL 
+//         //BUSCO ID DEL PRODUCTO
+
+//         let actualId = (e.target.id)
+//         console.log (actualId)
+
+
+//         //ENCONTRAR OBJETO ACTUAL 
 
         
-            const item =stockProductos.find((producto) => producto.id === actualId)
-            console.log (item)
+//             const item =stockProductos.find((producto) => producto.id === actualId)
+//             console.log (item)
            
             
             
@@ -61,33 +88,33 @@ agregarBtns.forEach (btn =>  {
 
 
 
-        // AGREGRAR EL PRODUCTO AL ARREGLO DEL CARRO
+//         // AGREGRAR EL PRODUCTO AL ARREGLO DEL CARRO
 
 
 
-    //AGREGO PRODUCTOS AL CARRITO
+//     //AGREGO PRODUCTOS AL CARRITO
       
     
-    modalBody.innerHTML += `
-    <div class="modal-contenedor">
-      <div>
-      <img class="img-fluid img-carrito" src=""/>
-      </div>
-      <div>
-      <p>Producto: </p>
-    <p>Precio: $ </p>
-    <p>Cantidad:</p>
-    <button class="btn btn-danger"  id= "">Eliminar producto</button>
-      </div>
-    </div>
+//     modalBody.innerHTML += `
+//     <div class="modal-contenedor">
+//       <div>
+//       <img class="img-fluid img-carrito" src=""/>
+//       </div>
+//       <div>
+//       <p>Producto: </p>
+//     <p>Precio: $ </p>
+//     <p>Cantidad:</p>
+//     <button class="btn btn-danger"  id= "">Eliminar producto</button>
+//       </div>
+//     </div>
     
-    `;
+//     `;
           
     
-          })
-      })
+//           })
+//       })
 
-})
+// })
 
 
 
